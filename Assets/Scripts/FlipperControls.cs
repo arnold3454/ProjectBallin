96a68a4265e07d1cde762405ecb17af584b2af79
Arnold Krajewski <arnoldkrajewski@aol.com>
2026-08-30 16:54:52 -0500

    Basic physics flippers and balls
    the left flipper is inverted for some reason

---
+++ Assets/Scripts/FlipperControls.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(HingeJoint))]
+public class FlipperControls : MonoBehaviour
+{
+    [SerializeField] private InputActionReference flipAction;
+
+    private HingeJoint hinge;
+    private float motorSpeed;
+
+    private void Awake()
+    {
+        hinge = GetComponent<HingeJoint>();
+        motorSpeed = Mathf.Abs(hinge.motor.targetVelocity);
+    }
+
+    private void OnEnable()
+    {
+        flipAction.action.performed += OnFlip;
+        flipAction.action.canceled += OnRelease;
+        flipAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        flipAction.action.performed -= OnFlip;
+        flipAction.action.canceled -= OnRelease;
+        flipAction.action.Disable();
+    }
+
+    private void OnFlip(InputAction.CallbackContext ctx) => SetDirection(motorSpeed);
+    private void OnRelease(InputAction.CallbackContext ctx) => SetDirection(-motorSpeed);
+
+    private void SetDirection(float targetVelocity)
+    {
+        JointMotor motor = hinge.motor;
+        motor.targetVelocity = targetVelocity; 
+        hinge.motor = motor;
+    }
+}
