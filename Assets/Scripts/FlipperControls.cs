10dc830dd49d94886146689de6eebe5967909042
NoMansion <gsam8189@gmail.com>
2026-09-07 10:47:55 -0500

    Update map scaling and pause UI

--- Assets/Scripts/FlipperControls.cs
+++ Assets/Scripts/FlipperControls.cs
@@ -7,14 +7,21 @@ public class FlipperControls : MonoBehaviour
     [SerializeField] private InputActionReference flipAction;
 
     private HingeJoint hinge;
+    private Rigidbody body;
     private float motorSpeed;
     private float baseMotorSpeed; // Added to store the original speed
+    private Vector3 baseWorldPosition;
+    private Vector3 baseConnectedAnchor;
 
     private void Awake()
     {
         hinge = GetComponent<HingeJoint>();
+        body = GetComponent<Rigidbody>();
+        body.useGravity = false;
         baseMotorSpeed = hinge.motor.targetVelocity; 
         motorSpeed = baseMotorSpeed;
+        baseWorldPosition = transform.position;
+        baseConnectedAnchor = hinge.connectedAnchor;
     }
 
     private void OnEnable()
@@ -45,5 +52,17 @@ public class FlipperControls : MonoBehaviour
     public void ScaleMotor(float multiplier)
     {
         motorSpeed = baseMotorSpeed * multiplier;
+    }
+
+    public void ApplyMapPosition(float mapScale, Vector3 mapOrigin, float horizontalInset, Vector3 offset)
+    {
+        Vector3 position = mapOrigin + (baseWorldPosition - mapOrigin) * mapScale;
+        position.x -= Mathf.Sign(baseWorldPosition.x - mapOrigin.x) * horizontalInset * mapScale;
+        position += offset * mapScale;
+        hinge.autoConfigureConnectedAnchor = false;
+        hinge.connectedAnchor = baseConnectedAnchor + (position - baseWorldPosition);
+        body.position = position;
+        body.WakeUp();
+        Physics.SyncTransforms();
     }
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -15,6 +15,11 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private float defaultScale = 1f;
     [SerializeField] private float largeScale = 1.2f;
 
+    [Header("Flipper Position Adjustments")]
+    [SerializeField] private float smallFlipperInset = 2f;
+    [SerializeField] private Vector3 smallFlipperOffset = new Vector3(0f, 0f, 0.5f);
+    [SerializeField] private Vector3 largeFlipperOffset = new Vector3(0f, 0f, -0.5f);
+
     [Header("Camera Zoom Multipliers")]
     [SerializeField] private float smallCameraZoom = 0.8f;
     [SerializeField] private float defaultCameraZoom = 1f;
@@ -51,11 +56,11 @@ public class MenuManager : MonoBehaviour
         Time.timeScale = isMenuOpen ? 0f : 1f;
     }
 
-    public void SetSmallMap() => ApplyScale(smallScale, smallCameraZoom);
-    public void SetDefaultMap() => ApplyScale(defaultScale, defaultCameraZoom);
-    public void SetLargeMap() => ApplyScale(largeScale, largeCameraZoom);
+    public void SetSmallMap() => ApplyScale(smallScale, smallCameraZoom, smallFlipperInset, smallFlipperOffset);
+    public void SetDefaultMap() => ApplyScale(defaultScale, defaultCameraZoom, 0f, Vector3.zero);
+    public void SetLargeMap() => ApplyScale(largeScale, largeCameraZoom, 0f, largeFlipperOffset);
 
-    private void ApplyScale(float mapScale, float cameraScale)
+    private void ApplyScale(float mapScale, float cameraScale, float flipperInset, Vector3 flipperOffset)
     {
         if (mapRoot != null) 
         {
@@ -75,9 +80,11 @@ public class MenuManager : MonoBehaviour
         }
 
         FlipperControls[] flippers = FindObjectsByType<FlipperControls>(FindObjectsInactive.Include);
+        Vector3 mapOrigin = mapRoot != null ? mapRoot.position : Vector3.zero;
         foreach (FlipperControls flipper in flippers)
         {
             flipper.ScaleMotor(mapScale);
+            flipper.ApplyMapPosition(mapScale, mapOrigin, flipperInset, flipperOffset);
         }
 
         ToggleMenu(); 
